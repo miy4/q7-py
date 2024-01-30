e054007a0bf9fdd50963bc4454d26f5b891e9120
miy4 <miy4@users.noreply.github.com>
2024-01-31 01:35:59 +0900

    Step 5: turn off echoing

--- src/q7_py/__main__.py
+++ src/q7_py/__main__.py
@@ -1,10 +1,21 @@
 import sys
+import termios
+
+
+def enable_raw_mode() -> None:
+    stdin_fileno = sys.stdin.fileno()
+    raw_mode = termios.tcgetattr(stdin_fileno)
+    lflag = 3
+    raw_mode[lflag] &= ~termios.ECHO
+
+    termios.tcsetattr(stdin_fileno, termios.TCSAFLUSH, raw_mode)
 
 
 def main() -> None:
+    enable_raw_mode()
+
     while sys.stdin.buffer.read(1) != b"q":
         pass
-
     sys.exit(0)
 
 
