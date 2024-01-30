81047f2ac69aea58454107569daff166bb5eda1b
miy4 <miy4@users.noreply.github.com>
2024-01-31 08:45:04 +0900

    Step 11: disable Ctrl-V

--- src/q7_py/__main__.py
+++ src/q7_py/__main__.py
@@ -20,7 +20,7 @@ class RawMode:
 
         iflag, lflag = 0, 3
         raw_mode[iflag] &= ~(termios.IXON)
-        raw_mode[lflag] &= ~(termios.ECHO | termios.ICANON | termios.ISIG)
+        raw_mode[lflag] &= ~(termios.ECHO | termios.ICANON | termios.ISIG | termios.IEXTEN)
 
         termios.tcsetattr(stdin_fileno, termios.TCSAFLUSH, raw_mode)
 
